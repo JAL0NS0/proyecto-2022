88d225b431f0bedf48cdffda4d14761c8635f1dd
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-21 13:11:14 -0600

    quitar estilo a

--- elementos/header-1.php
+++ elementos/header-1.php
@@ -1,7 +1,7 @@
 <header > 
     <div class='container-fluid d-flex' id="encabezado-titulos">
         <div id="logo" class="d-flex">
-            <a href="index.php">
+            <a href="index.php" id="casa">
                 <img src="./img/logo.png" alt="logo">
                 <h1>TUDI</h1> 
             </a>            
