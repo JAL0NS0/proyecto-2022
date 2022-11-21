7078a2a0a03a5839bb65fd06a28f4826c403e615
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-21 01:30:32 -0600

    quitamos echo de editar selector

--- control/editar-selector.php
+++ control/editar-selector.php
@@ -5,20 +5,16 @@
     $producto_id = $_SESSION['id'];
     $tipo_seleccion = $_POST['tipo_seleccion'];
     $id=$_GET["id"];
-    echo $selector;
 
     $query = "DELETE FROM tudi.selector WHERE id=".$id;
-    echo $query;
     if (!$conn->query($query) === TRUE) {
         echo "Error al actualizar la caracteristica $nombre";
         die();
     }
-    echo "Eliminado";
     
     $sql = "INSERT INTO `tudi`.`selector`(`id`,`tipo_selector`)
             VALUES ($id,$tipo_seleccion);";
 
-    echo $sql;
     if (!$conn->query($sql)) {
         echo "Error al insertar caracteristica: " . $sql . "<br>" . $conn->error;
         echo "<a href='editar-producto.php?id=$producto_id'> Salga de aquí </a>";
@@ -26,9 +22,6 @@
     }
 
     $selector_id=mysqli_insert_id($conn);
-    echo "... <br>";
-    echo "... <br>";
-    echo "Caracteristica creada exitosamente $selector_id <br>";
 
     if( isset( $_POST['descripciones'.$id] ) ){
         foreach ($_POST['descripciones'.$id] as $index => $descripcion) {
@@ -37,10 +30,7 @@
             $sql = "INSERT INTO `tudi`.`opcion` (`nombre`,`costo`,`selectorid`)
                 VALUES('$descripcion',$valor,$id);";
             
-            echo $sql."<br>";
-            if ($conn->query($sql) === TRUE) {
-                echo "Opcion $descripcion = $valor guardado correctamente.";
-            } else {
+            if (! $conn->query($sql)) {
                 echo "Error: " . $sql . "<br>" . $conn->error . " Guardando Opcion $descripcion = $valor guardado correctamente.";
             }
         }
