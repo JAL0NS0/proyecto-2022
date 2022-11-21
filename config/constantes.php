3b239907c277b4108adaf75e92eb79c3277f13c1
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-21 13:01:55 -0600

    correxion constantes tienda

--- config/constantes.php
+++ config/constantes.php
@@ -1,26 +1,32 @@
 <?php 
+    ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
     session_start();
 
     //Constantes
 
-    //conexión con db
-    $servername = "localhost";
-    $username = "root";
-    $password = "al0ns0";
-    $dbname = "tudi";
-
+    //Fecha actual
+    date_default_timezone_set('America/Guatemala');
     //Tipos de selectores y valores
     $nombresInput = array("Selector","Texto");
     $nombresSelector = array("Unico","Multiple");
-    $nombresValor = array("Base+Costo");
     $separacionesTexto = array("Por letra","Por palabra","Costo total");
-    
+
+    //conexión con db
+    $host= "tudi-server.mysql.database.azure.com";
+    $username = "admin4254";
+    $password = "proyecto-2022";
+    $db_name = "tudi";
+    $pagina = "https://tudi.azurewebsites.net";
+
     // Create connection
-    $conn = mysqli_connect($servername, $username, $password,$dbname);
+    $conn = mysqli_init(); 
+    mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+    
     
     // Check connection
-    if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
+    if(mysqli_connect_errno()){
+        die("Failed to connect to MySQL: ".mysqli_connect_error());
     }
     //echo "Connected successfully";
 ?>
