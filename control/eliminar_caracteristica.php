e26926ae756935fe84f765122fcbd958e32ffd39
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-22 00:26:29 -0600

    eliminación de extras

--- control/eliminar_caracteristica.php
+++ control/eliminar_caracteristica.php
@@ -5,7 +5,6 @@
 
     $query = "DELETE FROM tudi.caracteristica WHERE id=$id";
     if ($conn->query($query) === TRUE) {
-        echo "Eliminado";
         header("Location: editar-producto.php?id=".$_SESSION['id']);   
     } else {
         echo "Error al actualizar la caracteristica $nombre_car";
