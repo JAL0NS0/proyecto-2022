b04c7312b5cd82cdd96e38bbc4b30f22c3c2e5e5
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-22 00:20:29 -0600

    eliminación de extras

--- control/eliminar_categoria.php
+++ control/eliminar_categoria.php
@@ -6,7 +6,6 @@
     $query = "DELETE FROM tudi.categoria WHERE nombre='$categoria'";
 
     if ($conn->query($query) === TRUE) {
-        echo "Eliminado";
         header("Location: ./control.php");   
     } else {
         echo "Error al eliminar la caracteristica con codigo $codigo";
