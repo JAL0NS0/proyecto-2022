1f6695edf6b5f94188598978088ac5567301ab25
Joaquin Marroquin <joaquin.marroquin@galileo.edu>
2022-11-21 18:55:18 -0600

    pruebas eliminar producto

--- control/eliminar-producto.php
+++ control/eliminar-producto.php
@@ -1,5 +1,18 @@
 <?php
-    include("config/constantes.php");
+    ini_set('session.save_path',realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../session'));
+    session_start();
+
+
+    $conn = mysqli_init(); 
+    mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
+    
+    
+    // Check connection
+    if(mysqli_connect_errno()){
+        die("Failed to connect to MySQL: ".mysqli_connect_error());
+    }
+
     $id =$_GET["id"];
 
 
